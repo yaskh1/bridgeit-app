c2cfebc99e0a41d16f6d92ad880bc9bbdda89b43
yaskh1 <khalil.yasmeen2@gmail.com>
2026-08-13 21:18:08 +0300

    Add custom admin login redirect URL

--- bridgeit/settings.py
+++ bridgeit/settings.py
@@ -73,3 +73,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
+# BridgeIT Admin Custom Styling
+ADMIN_LOGIN_REDIRECT_URL = '/admin/'
