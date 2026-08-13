5b7c7bf8be1801fd4c923d547c7bcdc0cd9d32ec
yaskh1 <khalil.yasmeen2@gmail.com>
2026-08-13 21:18:38 +0300

    Add admin account creation to build script
    Added script to create initial BridgeIT Admin account if it doesn't exist.

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
@@ -7,3 +6,12 @@ pip install -r requirements.txt
 export PYTHONPATH=.
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create initial BridgeIT Admin account
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@bridgeit.local', 'BridgeIT2026!');
+    print('BridgeIT Admin Created Successfully');
+"
