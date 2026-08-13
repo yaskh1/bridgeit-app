848bcecaad6456327d8a9abcf7fd5615420ff0b4
yaskh1 <khalil.yasmeen2@gmail.com>
2026-08-13 21:17:31 +0300

    Redirect homepage to admin login and customize admin UI
    Added a redirect from the homepage to the admin login screen and customized the admin site header and titles.

--- bridgeit/urls.py
+++ bridgeit/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.shortcuts import redirect
+
+# Customize Admin Header & Title to BridgeIT
+admin.site.site_header = "BridgeIT Admin Portal"
+admin.site.site_title = "BridgeIT"
+admin.site.index_title = "Pass it over. Safely."
+
+# Redirect homepage directly to the Login Screen
+def redirect_to_login(request):
+    return redirect('/admin/login/')
 
 urlpatterns = [
+    path('', redirect_to_login),
     path('admin/', admin.site.urls),
 ]
